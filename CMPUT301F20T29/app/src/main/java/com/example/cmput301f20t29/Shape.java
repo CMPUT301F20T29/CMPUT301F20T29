e1ff4c9a420622a4249ff0e077d6fdb2c8647845
Tij Sachdeva <ks.sachdeva05@gmail.com>
2020-10-01 19:43:18 -0600

    Update Shape.java
    Update shape

--- CMPUT301F20T29/app/src/main/java/com/example/cmput301f20t29/Shape.java
+++ CMPUT301F20T29/app/src/main/java/com/example/cmput301f20t29/Shape.java
@@ -3,7 +3,7 @@ package com.example.cmput301f20t29;
 public abstract class Shape {
     private int x;
     private int y;
-    String color = "turquoise";
+    String color = "purple";
 
     public Shape(int x, int y) {
         this.x = x;
