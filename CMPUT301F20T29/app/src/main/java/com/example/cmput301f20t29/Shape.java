d2ce8f0eaa6a7e43d725931aa77fee0482aad0c6
yimage-web <59943772+yimage-web@users.noreply.github.com>
2020-10-01 18:40:22 -0600

    Update Shape.java

--- CMPUT301F20T29/app/src/main/java/com/example/cmput301f20t29/Shape.java
+++ CMPUT301F20T29/app/src/main/java/com/example/cmput301f20t29/Shape.java
@@ -3,6 +3,7 @@ package com.example.cmput301f20t29;
 public abstract class Shape {
     private int x;
     private int y;
+    String color = "turquoise";
 
     public Shape(int x, int y) {
         this.x = x;
