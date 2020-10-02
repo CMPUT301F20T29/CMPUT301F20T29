478f0f1fe0f0c3daa00b99aaaf743199f927928e
SolarVanity <lucas.ma.dev@gmail.com>
2020-10-01 18:45:57 -0600

    Update Shape.java

--- CMPUT301F20T29/app/src/main/java/com/example/cmput301f20t29/Shape.java
+++ CMPUT301F20T29/app/src/main/java/com/example/cmput301f20t29/Shape.java
@@ -3,7 +3,7 @@ package com.example.cmput301f20t29;
 public abstract class Shape {
     private int x;
     private int y;
-    private String color = "blue";
+    String color = "blue";
 
     public Shape(int x, int y) {
         this.x = x;
